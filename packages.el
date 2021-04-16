b5bdb22587bc827735947de42905a4d0dc34e0da
Dennie te Molder <dennie503@live.nl>
2021-04-16 12:36:42 +0200

    Added interaction-log-mode for debugging

--- packages.el
+++ packages.el
@@ -9,8 +9,6 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
-(package! org-superstar)
-
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
@@ -50,3 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+(package! org-superstar)
+(package! interaction-log)
