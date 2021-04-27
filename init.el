fa302784ae9eca9f86c8eca305c01f23b42eb444
Dennie te Molder <dennie503@live.nl>
2021-04-27 12:17:07 +0200

    Init cleanup

--- init.el
+++ init.el
@@ -20,98 +20,99 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
-       ;; +childframe)      ; improved UI
+       (company            ; the ultimate code completion backend
+        +childframe)       ; improved UI
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy               ; a search engine for love and life
-        +icons            ; icons are nice
-        ;;+fuzzy            ; fuzzy searching
-        +prescient)       ; I know what I want(ed)
+       (ivy                ; a search engine for love and life
+        +icons             ; icons are nice
+        ;;+fuzzy           ; fuzzy searching
+        +prescient)        ; I know what I want(ed)
 
        :ui
-       deft              ; notational velocity for Emacs
-       doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       deft                ; notational velocity for Emacs
+       doom                ; what makes DOOM look the way it does
+       doom-dashboard      ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
-       fill-column       ; a `fill-column' indicator
-       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       hydra             ; define transient state-like key bindings
-       indent-guides     ; highlighted indent columns
-       (ligatures        ; ligatures and symbols to make your code pretty again
+       fill-column         ; a `fill-column' indicator
+       hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       hydra               ; define transient state-like key bindings
+       indent-guides       ; highlighted indent columns
+       (ligatures          ; ligatures and symbols to make your code pretty again
         +extra)
-       minimap           ; show a map of the code on the side
-       modeline          ; snazzy, Atom-inspired modeline, plus API
-       nav-flash         ; blink cursor line after big motions
-       ;;neotree           ; a project drawer, like NERDTree for vim
-       ophints           ; highlight the region an operation acts on
-       (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
-        +defaults)       ; default popup rules
-       ;;tabs             ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       minimap             ; show a map of the code on the side
+       modeline            ; snazzy, Atom-inspired modeline, plus API
+       nav-flash           ; blink cursor line after big motions
+       ;;neotree             ; a project drawer, like NERDTree for vim
+       ophints             ; highlight the region an operation acts on
+       (popup              ; tame sudden yet inevitable temporary windows
+        +all               ; catch all popups that start with an asterix
+        +defaults)         ; default popup rules
+       ;;tabs              ; a tab bar for Emacs
+       treemacs            ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       vc-gutter         ; vcs diff in the fringe
-       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       (window-select    ; visually switch windows
+       vc-gutter           ; vcs diff in the fringe
+       vi-tilde-fringe     ; fringe tildes to mark beyond EOB
+       (window-select      ; visually switch windows
         +numbers)
-       workspaces        ; tab emulation, persistence & separate workspaces
-       zen               ; distraction-free coding or writing
+       workspaces          ; tab emulation, persistence & separate workspaces
+       zen                 ; distraction-free coding or writing
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       fold              ; (nigh) universal code folding
+       (evil +everywhere)  ; come to the dark side, we have cookies
+       file-templates      ; auto-snippets for empty files
+       fold                ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       multiple-cursors  ; editing in many places at once
+       multiple-cursors    ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
-       word-wrap         ; soft wrapping with language-aware indent
+       snippets            ; my elves. They type so I don't have to
+       word-wrap           ; soft wrapping with language-aware indent
 
        :emacs
-       (dired             ; making dired pretty [functional]
+       (dired              ; making dired pretty [functional]
         +ranger
         +icons)
-       electric          ; smarter, keyword-based electric-indent
-       (ibuffer         ; interactive buffer management
+       electric            ; smarter, keyword-based electric-indent
+       (ibuffer            ; interactive buffer management
         +icons)
-       undo              ; persistent, smarter undo for your inevitable mistakes
-       vc                ; version-control and Emacs, sitting in a tree
+       undo                ; persistent, smarter undo for your inevitable mistakes
+       vc                  ; version-control and Emacs, sitting in a tree
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       vterm               ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       (spell              ; tasing you for misspelling mispelling
+        +everywhere)
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible           ; a crucible for infrastructure as code
-       biblio            ; A collection of bibliographic reference tools
-       debugger          ; FIXME stepping through code, to help you add bugs
+       biblio              ; A collection of bibliographic reference tools
+       debugger            ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
-       (eval +overlay)     ; run code, run (also, repls)
+       eval                ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp
-       magit             ; a git porcelain for Emacs
+       magit               ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       pdf                 ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       rgb               ; creating color strings
+       rgb                 ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
