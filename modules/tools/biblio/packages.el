14243e55e28a45d9e31c102903fcfb22ee47b517
Dennie te Molder <dennie503@live.nl>
2021-05-02 22:26:10 +0200

    Org ref & Org Roam updates

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -7,6 +7,6 @@
 (when (featurep! :completion helm)
   (package! helm-bibtex :pin "ca09076c3d6e51cc4ffe208c8176fccf3710fcc6"))
 (when (featurep! :lang org)
-  (package! org-ref :pin "93f17b8a2cc9048a8df576f189cd5263662168db"))
+  (package! org-ref :pin "3ca9beb744621f007d932deb8a4197467012c23a"))
 (when (featurep! :lang org +roam)
-  (package! org-roam-bibtex :pin "068d9c27650207447ecf7a41fcd5b7abc40f4c3c"))
+  (package! org-roam-bibtex :pin "8d80bf980776df6ead53e917eb482ec8e309a1d7"))
