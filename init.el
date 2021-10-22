5bc69f5fb698e6060fe6c374ee2be9d695fe6312
Dennie te Molder <dennie503@live.nl>
2021-10-22 11:16:42 +0200

    General: cleanup

--- init.el
+++ init.el
@@ -37,16 +37,13 @@
        hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra               ; define transient state-like key bindings
        indent-guides       ; highlighted indent columns
-       (ligatures          ; ligatures and symbols to make your code pretty again
-        +extra)
+       (ligatures +extra)  ; ligatures and symbols to make your code pretty again
        minimap             ; show a map of the code on the side
        modeline            ; snazzy, Atom-inspired modeline, plus API
        nav-flash           ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints             ; highlight the region an operation acts on
-       (popup              ; tame sudden yet inevitable temporary windows
-       ;; +all             ; catch all popups that start with an asterix
-        +defaults)         ; default popup rules
+       (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        treemacs            ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
@@ -76,8 +73,7 @@
         +ranger
         +icons)
        electric            ; smarter, keyword-based electric-indent
-       (ibuffer            ; interactive buffer management
-        +icons)
+       (ibuffer +icons)    ; interactive buffer management
        undo                ; persistent, smarter undo for your inevitable mistakes
        vc                  ; version-control and Emacs, sitting in a tree
 
@@ -90,8 +86,7 @@
        :checkers
        (syntax             ; tasing you for every semicolon you forget
         +childframe)
-       (spell              ; tasing you for misspelling mispelling
-        +everywhere)
+       (spell +everywhere) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
