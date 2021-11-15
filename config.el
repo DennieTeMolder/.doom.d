fc791b70cc5d1ef9a1857712d22aa9e24bf08ea0
Dennie te Molder <dennie503@live.nl>
2021-11-15 20:21:21 +0100

    Emacs: second attempt at checking battery

--- config.el
+++ config.el
@@ -84,8 +84,9 @@
 (when IS-WSL (add-to-list 'default-frame-alist '(fullscreen . maximized)))
 
 ;; On laptops it's nice to know how much power you have
-(unless (equal "Battery status not available" (battery))
-  (display-battery-mode 1))
+(after! battery
+  (unless (equal "unknown" (cdr (assoc 66 (battery-upower))))
+    (display-battery-mode 1)))
 
 ;; Replace the default doom splash screen with amore subtle one
 (defun doom-dashboard-draw-ascii-banner-fn ()
