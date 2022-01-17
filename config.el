a1e2083c941630ad1f7747562aee6bbafef89da2
Dennie te Molder <dennie503@live.nl>
2022-01-17 16:28:13 +0100

    Doom: make "Z" bindings only kill current buffer

--- config.el
+++ config.el
@@ -190,6 +190,11 @@
       :desc "Repeat last command" "r" #'repeat
       :desc "Adjust windows hydra" "w a" #'+hydra/window-nav/body)
 
+;; Make "Z" bindings only kill buffers not the session
+(map! :map evil-normal-state-map
+      "Z Q" #'kill-buffer-and-window
+      "Z Z" #'doom/save-and-kill-buffer)
+
 ;; Use mouse buttons to go forward/backward trough window configs
 (map! :n [mouse-8] #'winner-undo
       :n [mouse-9] #'winner-redo
