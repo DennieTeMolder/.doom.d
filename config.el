791531a3dd263aede73fb5aa26bc8467f7b833b0
Dennie te Molder <dennie503@live.nl>
2022-01-20 17:58:28 +0100

    Evil-mc: clear additional keymap to keep evil-pop

--- config.el
+++ config.el
@@ -246,6 +246,15 @@
   ;; Disable company auto pop-up in org, use C-SPC to trigger
   (add-hook! 'org-mode-hook (setq-local company-idle-delay nil)))
 
+;; `evil-goggles--show-p' loads evil-mc causing its keymap to overwrite the
+;; evil-pop commands, clear this keybinding to keep them available.
+(after! evil-mc
+  (map! :map evil-mc-key-map
+        :nv "C-n" nil
+        :nv "C-N" nil
+        :nv "C-p" nil
+        :nv "C-P" nil))
+
 (after! projectile
   ;; Projectle sorting by recently opened
   (setq projectile-sort-order 'recently-active)
