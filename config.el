df850253bfeac770d085b8b721d1d875a76693de
Dennie te Molder <dennie503@live.nl>
2022-06-19 16:12:41 +0200

    Doom: eval eslisp in *scratch* merged upstream

--- config.el
+++ config.el
@@ -1050,24 +1050,3 @@ block, send the entire code block."
                     indent-tools-goto-child
                     indent-tools-goto-end-of-tree))
     (advice-add symbol :around #'doom-set-jump-maybe-a)))
-
-(defadvice! my/fix-emacs-lisp-eval (beg end)
-  "Fixes a type error when evaluating elisp in the *scratch* buffer"
-  :override #'+emacs-lisp-eval
-  (+eval-display-results
-   (string-trim-right
-    (condition-case-unless-debug e
-        (let ((result
-               (let* ((buffer-file-name (buffer-file-name (buffer-base-buffer)))
-                      (buffer-file-truename (when buffer-file-name
-                                              (file-truename buffer-file-name)))
-                      (doom--current-module
-                       (ignore-errors (doom-module-from-path buffer-file-name)))
-                      (debug-on-error t))
-                 (eval (read (format "(progn %s)"
-                                     (buffer-substring-no-properties beg end)))
-                       lexical-binding))))
-          (require 'pp)
-          (replace-regexp-in-string "\\\\n" "\n" (pp-to-string result)))
-      (error (error-message-string e))))
-   (current-buffer)))
