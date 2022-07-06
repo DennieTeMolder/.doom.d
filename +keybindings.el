5631e5bdf8a7e7d3893fc49686b07689c87c2132
Dennie te Molder <dennie503@live.nl>
2022-07-06 23:47:02 +0200

    Doom: format map! for readability

--- +keybindings.el
+++ +keybindings.el
@@ -3,13 +3,13 @@
 ;;; <leader>
 (map! :leader
       (:when (featurep! :ui zen-light)
-       :desc "Zen writing mode" "t z" #'visual-fill-column-mode)
+       :desc "Zen writing mode"     "t z" #'visual-fill-column-mode)
       (:when (featurep! :ui popper)
        "/" nil ; Unbind `+default/search-project' (also bound to "SPC s p")
        (:prefix ("/" . "popup")
-        :desc "Show/hide" "/" #'popper-toggle-latest
-        :desc "Next" "n" #'popper-cycle
-        :desc "Kill" "k" #'my/popper-kill-latest-popup-keep-open
-        :desc "Quit" "q" #'popper-kill-latest-popup
+        :desc "Show/hide"           "/" #'popper-toggle-latest
+        :desc "Next"                "n" #'popper-cycle
+        :desc "Kill"                "k" #'my/popper-kill-latest-popup-keep-open
+        :desc "Quit"                "q" #'popper-kill-latest-popup
         :desc "Toggle popup/buffer" "t" #'my/popper-toggle-type
-        :desc "Raise" "r" #'my/popper-raise-popup)))
+        :desc "Raise"               "r" #'my/popper-raise-popup)))
