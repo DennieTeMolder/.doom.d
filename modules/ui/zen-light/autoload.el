e6f2aac4bf8b6738c76f031d4c47386f33f660c9
Dennie te Molder <dennie503@live.nl>
2022-07-07 14:31:53 +0200

    Zen-light: smarter restore line-numbers fun

--- modules/ui/zen-light/autoload.el
+++ modules/ui/zen-light/autoload.el
@@ -1,17 +1,18 @@
 ;;; ui/zen-light/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(defvar +zenl-display-line-numbers-previous-state t)
+(defvar +zenl-display-line-numbers-previous-state nil)
 
 ;;;###autoload
 (defun +zenl/hide-line-numbers ()
   "Hides line numbers while recording `+zenl-display-line-numbers-previous-state'"
   (interactive)
-  (setq-local +zenl-display-line-numbers-previous-state display-line-numbers)
-  (setq-local display-line-numbers nil))
+  (setq-local +zenl-display-line-numbers-previous-state display-line-numbers
+              display-line-numbers nil))
 
 ;;;###autoload
 (defun +zenl/restore-line-numbers ()
   "Restores line numbers to `+zenl-display-line-numbers-previous-state'"
   (interactive)
-  (setq-local display-line-numbers +zenl-display-line-numbers-previous-state))
+  (when +zenl-display-line-numbers-previous-state
+    (setq-local display-line-numbers +zenl-display-line-numbers-previous-state)))
