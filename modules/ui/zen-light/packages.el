36c55a37f1ae97f80909d3a42df4e08fbf1da2da
Pyrron <pyrron@HP_Envy_13>
2022-07-28 13:33:36 +0200

    Zen-light: pin packages

--- modules/ui/zen-light/packages.el
+++ modules/ui/zen-light/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/zen-light/packages.el
 
-(package! visual-fill-column)
-(package! mixed-pitch)
+(package! visual-fill-column :pin "453d698d7fc243a547665f8ba43c55eee574e0db")
+(package! mixed-pitch :pin "519e05f74825abf04b7d2e0e38ec040d013a125a")
