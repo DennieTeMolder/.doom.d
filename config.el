c80e26b788d7400f10dcf1d2a5228fdac6e58727
DennieTeMolder <dennie503@live.nl>
2022-08-03 14:11:59 +0200

    Revert "Emacs: enable subword-mode in prog-mode"
    This reverts commit 95a3cf6c5a1c7730c5fc049975516526ca0cb6b4.

--- config.el
+++ config.el
@@ -576,10 +576,6 @@ Also used by `org-modern-mode' to calculate heights.")
   (setq-default TeX-master nil))
 
 ;;;; Programming Languages
-;; Treat sub-words in camelCased words as individual text objects
-(after! prog-mode
-  (add-hook! 'prog-mode-hook #'subword-mode))
-
 ;; General interactive programming buffer settings
 (after! comint
   (setq ansi-color-for-comint-mode 'filter
