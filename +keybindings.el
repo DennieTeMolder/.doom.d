c29cf0f3a0d7944bafe9b2398504691b4cfdd6ae
DennieTeMolder <dennie503@live.nl>
2022-08-10 10:26:49 +0200

    Keybindings: do not check for custom modules

--- +keybindings.el
+++ +keybindings.el
@@ -57,10 +57,10 @@
 
 ;;; Custom modules
 (map! :leader
-      (:when (featurep! :ui zen-light)
-       :desc "Zen writing mode"     "t z" #'visual-fill-column-mode)
+      ;; :when (featurep! :ui zen-light)
+       :desc "Zen writing mode"     "t z" #'visual-fill-column-mode
 
-      (:when (featurep! :ui popper)
+      ;; :when (featurep! :ui popper)
        "/" nil ; Unbind `+default/search-project' (also bound to "SPC s p")
        (:prefix ("/" . "popup")
         :desc "Show/hide"           "/" #'popper-toggle-latest
@@ -68,4 +68,4 @@
         :desc "Kill"                "k" #'+popper/kill-latest-popup-keep-open
         :desc "Quit"                "q" #'popper-kill-latest-popup
         :desc "Toggle popup/buffer" "t" #'+popper/toggle-type
-        :desc "Raise"               "r" #'+popper/raise-popup)))
+        :desc "Raise"               "r" #'+popper/raise-popup))
