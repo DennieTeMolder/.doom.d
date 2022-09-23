7811982e53941e0f5737a3aa645dba4043620be6
DennieTeMolder <dennie503@live.nl>
2022-09-23 17:07:02 +0200

    Zen-light: fix text scale toggle on mode hook

--- modules/ui/zen-light/autoload.el
+++ modules/ui/zen-light/autoload.el
@@ -1,6 +1,16 @@
 ;;; ui/zen-light/autoload.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
+(defun +zenl-visual-fill-column-h ()
+  "Toggles additional adjustments to accompany `visual-fill-column-mode'."
+      (text-scale-set (if visual-fill-column-mode +zenl-text-scale 0))
+      (display-line-numbers-mode (if visual-fill-column-mode -1 1))
+      (when (and (eq major-mode 'org-mode)
+                 (modulep! :lang org))
+        (+org-pretty-mode (if visual-fill-column-mode 1 -1)))
+      (visual-fill-column-adjust))
+
+;;;###autoload
 (defun +zenl-enable-mixed-pitch-mode-h ()
   "Enable `mixed-pitch-mode' when in `+zenl-mixed-pitch-modes'."
   (when (apply #'derived-mode-p +zenl-mixed-pitch-modes)
--- modules/ui/zen-light/config.el
+++ modules/ui/zen-light/config.el
@@ -9,22 +9,13 @@
 
 (use-package! visual-fill-column
   :commands visual-fill-column-mode
+  :hook (visual-fill-column-mode . +zenl-visual-fill-column-h)
   :config
   ;; Text scaling is bugged: https://codeberg.org/joostkremers/visual-fill-column/issues/1
   (setq visual-fill-column-width 100
         visual-fill-column-center-text t
         visual-fill-column-extra-text-width '(-5 . 5)
-        visual-fill-column-adjust-for-text-scale nil)
-
-  (add-hook! 'visual-fill-column-mode-hook
-    (defun +zenl-visual-fill-column-h ()
-        (display-line-numbers-mode (if visual-fill-column-mode -1 +1))
-        (when (+zenl-enable-mixed-pitch-mode-h)
-          (text-scale-set (if visual-fill-column-mode +zenl-text-scale 0)))
-        (when (and (eq major-mode 'org-mode)
-                   (modulep! :lang org))
-          (+org-pretty-mode (if visual-fill-column-mode +1 -1)))
-        (visual-fill-column-adjust))))
+        visual-fill-column-adjust-for-text-scale nil))
 
 (use-package! mixed-pitch
   :hook (visual-fill-column-mode . +zenl-enable-mixed-pitch-mode-h)
