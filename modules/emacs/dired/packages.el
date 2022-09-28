dacfb48fabe40131101f49a63c8c61f7a9400dba
DennieTeMolder <dennie503@live.nl>
2022-09-28 10:21:15 +0200

    Dirvish: bump package

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -2,4 +2,4 @@
 ;;; emacs/dired/packages.el
 
 (package! diredfl :pin "62b559e1d6b69834a56a57eb1832ac6ad4d2e5d0")
-(package! dirvish :pin "26766d608145a43a3602d165f88ec01eea1bca24")
+(package! dirvish :pin "fb5bedf123266eaccbfc66d45582f4158e3870b2")
