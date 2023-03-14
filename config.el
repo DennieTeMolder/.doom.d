80c660eb7c8d95337ff7a57adebb868b9d5cd2ed
DennieTeMolder <dennie503@live.nl>
2023-03-14 16:14:54 +0100

    Vundo: disable misaligned unicode symbols

--- config.el
+++ config.el
@@ -431,7 +431,6 @@
 (use-package! vundo
   :commands vundo
   :config
-  (setq vundo-glyph-alist vundo-unicode-symbols)
   (map! :map vundo-mode-map
         "h" #'vundo-backward
         "j" #'vundo-next
