482bc317a03eb196dd5269b88005d2a53b550400
DennieTeMolder <dennie503@live.nl>
2023-05-03 00:42:55 +0200

    Zen-light: respect display-line-numbers-mode

--- modules/ui/zen-light/autoload/zen-light.el
+++ modules/ui/zen-light/autoload/zen-light.el
@@ -3,17 +3,15 @@
 ;;;###autoload
 (defun +zen-light-toggle (&optional arg)
   "Toggle zen/focus mode. Uses `+zenl-reading-modes' & `+zenl-reading-scale'."
-  (interactive)
+  (interactive
+   (list (when current-prefix-arg (prefix-numeric-value current-prefix-arg))))
   (setq arg (if (numberp arg)
                 (< 0 arg)
               (not (bound-and-true-p fill-column-center-mode))))
   (when (apply #'derived-mode-p +zenl-reading-modes)
     (text-scale-set (if arg +zenl-reading-scale 0))
     (mixed-pitch-mode (if arg 1 0)))
-  (if arg
-      (setq display-line-numbers nil)
-    (unless display-line-numbers
-      (setq display-line-numbers display-line-numbers-type)))
+  (display-line-numbers-mode (if arg 0 1))
   (when (boundp 'vi-tilde-fringe-mode)
     (vi-tilde-fringe-mode (if arg 0 1)))
   (fill-column-center-mode (if arg 1 0)))
