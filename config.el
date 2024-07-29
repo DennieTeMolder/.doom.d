b4bc91fcf77af905e5ddd8ab7e75b820e79ced03
DennieTeMolder <dennie503@live.nl>
2024-07-29 11:40:46 +0200

    org-modern: re-enable table as alignment is fixed
    This was patched upstream last month

--- config.el
+++ config.el
@@ -638,7 +638,6 @@ Also used by `org-modern-mode' to calculate heights.")
   (setq org-modern-label-border dtm-org-line-spacing
         org-modern-hide-stars (org-add-props " " nil 'face 'org-indent)
         org-modern-statistics nil
-        org-modern-table nil
         org-modern-star ["●" "◉" "○" "◉" "○" "◉" "○" "◉"]
         org-modern-list '((?+ . "›")
                           (?- . "‒")
